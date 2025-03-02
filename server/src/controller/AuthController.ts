cc4e772efa6bfbb17f1e350c32d3a5b172100a3f
oyavri <oyavri@gmail.com>
2025-03-02 21:26:22 +0300

    Implement register and login for user, WIP

--- server/src/controller/AuthController.ts
+++ server/src/controller/AuthController.ts
@@ -1,8 +1,10 @@
 import { Router } from "@oak/oak/router";
-import { PoolClient, PostgresError } from "https://deno.land/x/postgres@v0.19.3/mod.ts";
+import { PoolClient } from "https://deno.land/x/postgres@v0.19.3/mod.ts";
 import { Status } from "https://jsr.io/@oak/oak/17.1.4/deps.ts";
 import { UserModel } from "../model/UserModel.ts";
-import { registerUser } from "../service/AuthenticationService.ts";
+import { loginUser, registerUser } from "../service/AuthenticationService.ts";
+import { UserAlreadyExistsError } from "../model/UserAlreadyExistsError.ts";
+import { UserNotFoundError } from "../model/UserNotFoundError.ts";
 
 export const authRouter = new Router();
 
@@ -31,20 +33,69 @@ authRouter.post("/register", async (ctx) => {
   const dbConnection: PoolClient = ctx.state.db.connection;
 
   try {
-    const registeredUser = registerUser(dbConnection, userCandidate);
+    const registeredUser = await registerUser(dbConnection, userCandidate);
+
     ctx.response.body = {
         "message": "User created successfully!",
     }
     ctx.response.status = Status.Created;
     ctx.response.type = "application/json";
+
+    dbConnection.release();
     return;
   } catch (error) {
     // user already exists!
-    if (error instanceof PostgresError && error.fields.code == "23505") {
-      console.error("Multiple registration on same username");
-      console.error(error);
+    if (error instanceof UserAlreadyExistsError) {
+      ctx.response.status = Status.Conflict;
+      ctx.response.type = "application/json";
+      ctx.response.body = {
+        "error": "User is already registered"
+      };
+
+      dbConnection.release();
+      return;
     }
+
+    dbConnection.release();
     throw error;
   }
 });
-// authRouter.post("/login", async (ctx) => {});
+
+authRouter.post("/login", async (ctx) => {
+  if (!ctx.request.hasBody) {
+    ctx.throw(Status.BadRequest, "Bad Request");
+  }
+
+  const body = ctx.request.body;
+  if (body.type() !== "json") {
+    ctx.throw(Status.BadRequest, "Unsupported format, only JSON is supported");
+  }
+
+  const json = await body.json();
+
+  if (!json.username || !json.password) {
+    ctx.throw(Status.BadRequest, "Username and password required");
+  }
+
+  const dbConnection: PoolClient = ctx.state.db.connection;
+  try {
+    const user = loginUser(dbConnection, json.username, json.password);
+    ctx.response.body = {
+      "message": "User logged in successfully",
+    }
+    ctx.response.status = Status.OK;
+    ctx.response.type = "application/json";
+
+    return;
+  } catch (error) {
+    if (error instanceof UserNotFoundError) {
+      ctx.response.status = Status.NotFound;
+      ctx.response.type = "application/json";
+      ctx.response.body = {
+        "error": "User not found"
+      };
+
+    }
+    console.error(error);
+  }
+});
--- server/src/service/AuthenticationService.ts
+++ server/src/service/AuthenticationService.ts
@@ -1,6 +1,8 @@
 import { PoolClient, PostgresError } from "https://deno.land/x/postgres@v0.19.3/mod.ts";
 import { UserModel } from "../model/UserModel.ts";
+import { UserAlreadyExistsError } from "../model/UserAlreadyExistsError.ts";
 import * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
+import { UserNotFoundError } from "../model/UserNotFoundError.ts";
 
 export async function registerUser(dbConnection: PoolClient, userCandidate: UserModel) {
   try {
@@ -8,22 +10,47 @@ export async function registerUser(dbConnection: PoolClient, userCandidate: User
   
     const createdUser = await dbConnection.queryObject<UserModel>({
       camelCase: true,
-      text: "INSERT INTO user (username, email, password) VALUES ($username, $email, $password);",
+      text: "INSERT INTO users (username, email, password) VALUES ($username, $email, $password);",
       args: { 
         username: userCandidate.username, 
         email: userCandidate.email, 
-        password: hashedPassword 
+        password: hashedPassword
       }
     });
 
+    console.log(createdUser);
+
     return createdUser.rows[0];
   } catch (error) { 
-      if (error instanceof PostgresError) {
-        console.log("----------------------");
-        console.error(error);
-        console.log("----------------------");
+      if (error instanceof PostgresError && error.fields.code == "23505") {
+        console.error("Multiple registration on same username");
+        throw new UserAlreadyExistsError("User already exists");
       }
 
       throw error;
+  }
+}
+
+export async function loginUser(dbConnection: PoolClient, username: string, password: string) {
+  try {
+    const userCandidate = await dbConnection.queryObject<UserModel>({
+      camelCase: true,
+      text: "SELECT * FROM users WHERE username = $username",
+      args: {
+        username: username
+      }
+    });
+
+    if (userCandidate.rows[0] === undefined) {
+      throw new UserNotFoundError("User does not exist");
+    }
+
+    const isValid = await bcrypt.compare(password, userCandidate.rows[0].password);
+    console.log(userCandidate);
+    console.log(isValid);
+    
+    return userCandidate.rows[0];
+  } catch (error) {
+      console.error(error);
   }
 }
